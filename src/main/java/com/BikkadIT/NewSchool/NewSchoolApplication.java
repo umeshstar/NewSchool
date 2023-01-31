8256ff44f008fc6b669f56f5ede9b19498be3537
umeshstar <umeshahire57@gmail.com>
2023-01-31 15:52:36 +0530

    changes by umesh

---
+++ src/main/java/com/BikkadIT/NewSchool/NewSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.NewSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/NewSchool/NewSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.NewSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
